7b6931bbb44626b73d77526c6e49234d8dbe0b94
Ryan Kwok <kaj.kwok@gmail.com>
2022-04-19 23:40:16 -0700

    completed displaying appointments

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  function reset() {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  function cancel() {
+    props.onCancel();
+    reset();
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(e) => setStudent(e.target.value)}
+            value={student}
+          />
+        </form>
+        <InterviewerList
+          value={interviewer}
+          onChange={setInterviewer}
+          interviewers={props.interviewers}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={() => cancel()}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 import "./styles.scss";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {props.time ? `Appointment at ${props.time}` : "No Appointments"}
+      <Header time={props.time} />
+        {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
     </article>
   )
 }
